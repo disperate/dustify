6bc6c98fe7b28d46d4973b465b87f71871fbece7
disperate <julian.bigler@hotmail.com>
2017-08-16 15:13:17 +0200

    Empty android app

---
+++ app/src/main/java/nl/hva/summeracademy/dustify/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.hva.summeracademy.dustify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
